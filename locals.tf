700afb100d2b451f60f7d7bf754142b9fe6e8a62
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-02 13:34:41 +0530

    updated to tf 12

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
 locals {
-  record_set_count = "${var.record_set_count}"
+  record_set_count = var.record_set_count
 }
+
+https://github.com/QuiNovas/terraform-aws-ecr.git
--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
 resource "aws_route53_zone" "zone" {
-  delegation_set_id = "${var.delegation_set_id}"
+  delegation_set_id = var.delegation_set_id
   lifecycle {
     prevent_destroy = true
   }
-  name              = "${var.name}"
+  name = var.name
 }
 
 resource "aws_route53_record" "record" {
-  count   = "${local.record_set_count}"
-  name    = "${lookup(var.record_set[count.index], "name")}"
-  records = [
-    "${split(",", lookup(var.record_set[count.index], "records"))}"
-  ]
-  ttl     = "${lookup(var.record_set[count.index], "ttl", "300")}"
-  type    = "${lookup(var.record_set[count.index], "type")}"
-  zone_id = "${aws_route53_zone.zone.zone_id}"
+  count   = local.record_set_count
+  name    = var.record_set[count.index]["name"]
+  records = split(",", var.record_set[count.index]["records"])
+  ttl     = lookup(var.record_set[count.index], "ttl", "300")
+  type    = var.record_set[count.index]["type"]
+  zone_id = aws_route53_zone.zone.zone_id
 }
+
